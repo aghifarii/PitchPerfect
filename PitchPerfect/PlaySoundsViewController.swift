ea66e2d3928131995fbef80dc7383bb65799e1f9
Alhamdani <alhamdanighifari@gmail.com>
2021-06-29 13:48:55 +0700

    test\

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -14,7 +14,7 @@ class PlaySoundsViewController: UIViewController {
 
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
-    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var 	rabbitButton: UIButton!
     @IBOutlet weak var vaderButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
